53ec66f77cf3d2b3a882e91dd8908cc8dd27883c
phantomxworld765 <phantomxworld765@gmail.com>
2026-01-10 23:53:46 +0530

    Update update_bot.py

--- update_bot.py
+++ update_bot.py
@@ -52,3 +52,17 @@ while True:
         bot.infinity_polling()
     except:
         time.sleep(5)
+from flask import Flask
+import threading
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot is running!"
+
+def run():
+    app.run(host='0.0.0.0', port=5000)
+
+# Bot ko thread mein run karo
+threading.Thread(target=run).start()
